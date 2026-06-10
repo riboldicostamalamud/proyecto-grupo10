64817a10c196830c65ac8a6288fbac17001a374b
Juan Cruz Riboldi <108473886+Juan-Riboldi@users.noreply.github.com>
2026-06-10 18:32:47 +0000

    implementacion de grafico de ship_mode y correcion en lectura de archivos

--- proyecto.py
+++ proyecto.py
@@ -1,24 +1,20 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 
-"""
-nombre_archivo: str
-claves: list[str]
-datos: list[str]
-fila2: dict[str, str]
-
-leer_archivo: str -> list
-
-lee el dataset y devuelve una lista de diccionarios, donde cada diccionario
-representa una fila del archivo, se utilizan los nombres de la columnas
-como claves del diccionario
-"""
+#nombre_archivo: str
+#claves: list[str]
+#datos: list[str]
+#fila2: dict[str, str]
+#leer_archivo: str -> list
+#lee el dataset y devuelve una lista de diccionarios, donde cada diccionario
+#representa una fila del archivo, se utilizan los nombres de la columnas
+#como claves del diccionario
 def leer_archivo(nombre_archivo):
     archivo = open(nombre_archivo, "r")
     lista = []
 
     claves = archivo.readline()
-    claves = encabezado.split(",")
+    claves = claves.split(",")
 
     for fila in archivo:
         datos = fila.split(",")
@@ -35,13 +31,67 @@ def leer_archivo(nombre_archivo):
     return lista
 
 
+#contador: dict
+#contar_ship_mode : list[dict] -> dict[str, int]
+#dada una lista con los datos, devuelve un diccionario donde cada clave es un
+#metodo de envio("ship mode") y su valor es la cantidad de veces que se uso
+def contar_ship_mode(lista):
+    contador = {}
+
+    for linea in lista:
+        ship_mode = linea["Ship Mode"]
+
+        if ship_mode in contador:
+            contador[ship_mode] += 1
+        else:
+            contador[ship_mode] = 1
+        
+    return contador
+
+
+#porcentajes: dict[str, float]
+#total: int
+#porcentaje: float
+#calcular_porcentajes_ship_mode: dict[str, int] -> list[float]
+#dado un diccionario que contiene los metodos de envio y sus cantidades, devuelve un diccionario
+#donde las claves son los metodo de envio y los valores son los porcentajes de utilizacion de cada metodo
+def calcular_porcentajes_ship_mode(contador):
+    porcentajes = {}
+    total = 0
+    for cantidad in contador.values():
+        total += cantidad
+
+    for metodo in contador:
+        porcentaje = contador[metodo] * 100 / total
+        porcentajes[metodo] = porcentaje
+    
+    return porcentajes
+        
+#contador: dict[str, int]
+#porcentajes: dict[str, float]
+#mostrar_grafico_ship_mode: list[dict] -> None
+#dada una lista con los registros, calcula la cantidad y el porcentaje de utilizacion
+#de cada metodo de envio y muestre un grafico circular usando Matplotlib
+def mostrar_grafico_ship_mode(registros):
+
+    contador = contar_ship_mode(registros)
+    porcentajes = calcular_porcentajes_ship_mode(contador)
+
+    labels = porcentajes.keys()
+    sizes = porcentajes.values()
+    fig, ax = plt.subplots()
+    ax.pie(sizes, labels=labels, autopct='%1.1f%%', textprops={'size': 'smaller'}, radius=0.75)
+    ax.set_title("¿Cuál es el porcentaje de utilizacion de cada envio?")
+    st.pyplot(fig)
 
 
 def main():
     st.title ("Proyecto Grupal de Programación")
-    st.write ("Empecemos a trabajar equipo!")
 
+    registros = leer_archivo("SampleSuperstore_geo.csv")
 
+    mostrar_grafico_ship_mode(registros)
 
+    
 if __name__ == "__main__":
     main()
